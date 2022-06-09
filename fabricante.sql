d3b91dfd9b537470cb252426b9aa6da42c1f972e
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-09 18:34:21 -0500

    Agregar archivos de proyecto.

---
+++ fabricante.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[fabricante]
+(
+	[codigo] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [nombre] VARCHAR(50) NOT NULL
+)
+
+GO
---
+++ producto.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[producto]
+(
+	[codigo] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [nombre] VARCHAR(50) NULL, 
+    [precio] FLOAT NULL, 
+    [codigo_fabricante] INT NULL,
+    FOREIGN KEY (codigo_fabricante) REFERENCES fabricante(codigo)
+)
